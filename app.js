e0826d8eeeb0a6d7c30ffb6f2f062ac751eeb456
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2022-10-24 21:38:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use((req,res,next)=>{
                          res.setHeader("Access-Control-Allow-Headers","Content-Type");
                          next();
                          });
-
+app.get((req,res,next)=>{
+                         console.log(req.body.asd);
+                         res.send("hello man");
+                         res.end();
+                         });
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
