fa3fca0142bccf58c90ddc9c8ed25adfd8df439b
BAHAA EDDIN AL-NOKTA <98487604+BAHAA-THE-KING@users.noreply.github.com>
2022-10-27 16:03:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ exports.connect=()=>{
                                    .connect("mongodb+srv://BAHAA:1C11BiuGJItWGteC@shop.qhv52.mongodb.net/?retryWrites=true&w=majority")
                                    .then(client=>{
                                                   console.log("DB Is Connected !");
-                                                  db=client.db();
+                                                  db=client.db("shop");
                                                   });
                      };
 exports.getDB=()=>{
