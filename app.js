8d2ab21ac23994bd38abdfd6ba2e22c6fd8b7c5f
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2022-10-27 14:26:25 +0300

    add db

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express=require("express");
+const db=require("./db");
 const app=express();
 app.get("/",(req,res,next)=>{
-                             res.send("hallo man !");
-                             res.end();
+                             const name=req.query.name;
+                             const password=req.query.password;
+                             db.getDB()
+                               .collection("users")
+                               .findOne({name})
+                               .then(user=>{
+                                            if (!user){
+                                              res.send("Wrong Name");
+                                              res.end();
+                                              return;
+                                              }
+                                            if (user.password!==password){
+                                              res.send("Wrong Password");
+                                              res.end();
+                                              return;
+                                              }
+                                            res.send("o;;bob;u;");
+                                            res.end();
+                                            });
                              });
 const port=process.env.PORT||3001;
-app.listen(port,()=>console.log("Listening On Port "+port));
+db.connect()
+  .then(()=>{
+             app.listen(port,()=>console.log("Listening On Port "+port));
+             })
+  .catch(err=>console.log(err));
---
+++ db.js
@@ -0,0 +1,13 @@
+const mongodb=require("mongodb");
+let db;
+exports.connect=()=>{
+                     return mongodb.MongoClient
+                                   .connect("mongodb+srv://BAHAA:1C11BiuGJItWGteC@shop.qhv52.mongodb.net/?retryWrites=true&w=majority")
+                                   .then(client=>{
+                                                  console.log("DB Is Connected !");
+                                                  db=client.db();
+                                                  });
+                     };
+exports.getDB=()=>{
+                   return db||console.log("ERROR DB");
+                   };
